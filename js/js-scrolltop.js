81271a6e7af9a705c3565beec547395341730ef3
Phan Đăng Khoa <khoaphan27102000@gmail.com>
2022-11-17 18:54:18 +0700

    up

--- js/js-scrolltop.js
+++ js/js-scrolltop.js
@@ -1,7 +1,8 @@
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
+    var height = $(window).innerHeight();
     $(".olipop").css({
-        width: ' ' + (50 + scroll /20) + '% '
+        width: ' ' + (50 + (scroll / height)*50) + '% '
     });
 });
 
