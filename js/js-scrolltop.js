b0a43799139d6f0ee77035a9224abce75bd12193
Phan Đăng Khoa <khoaphan27102000@gmail.com>
2022-11-17 17:56:39 +0700

    New constructure

--- js/js-scrolltop.js
+++ js/js-scrolltop.js
@@ -1,6 +1,7 @@
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
-    $(".firstview").css({
-        transform: 'translate3d(-50%, ' + (-50 + scroll / 100) + '%, 0) scale(' + (100 + scroll / 10) / 100 + ')'
+    $(".olipop").css({
+        width: ' ' + (50 + scroll /20) + '% '
     });
 });
+
