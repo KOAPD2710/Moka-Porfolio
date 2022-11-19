2563bd0eed34e8ab467748208d5ce649e1b68244
Phan Đăng Khoa <khoaphan27102000@gmail.com>
2022-11-19 18:29:13 +0700

    Up

--- js/js-scrolltop.js
+++ js/js-scrolltop.js
@@ -1,8 +1,32 @@
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     var height = $(window).innerHeight();
+    var up = scroll / height;
+    var numlock = 30;
+    /* Up run form 1 and up to ...*/
+
     $(".olipop").css({
-        width: ' ' + (50 + (scroll / height)*50) + '% '
+        width: ' ' + (50 + (up)*50) + '% '
     });
+
+    if ( up >= 1) {
+        $(".olipop").css({
+            transform: 'translate3d(-50%, ' + ((-50 - (numlock)) + (up)*(numlock)) + '%, 0)'
+        });
+        document.body.style.backgroundColor = 'var(--gr)';
+    } else{
+        document.body.style.backgroundColor = 'black';
+    }
 });
 
+$(document).ready(function(val){
+    $('.navisticky .navlinksm .menuicon').click(function(val){
+        if ( $(this).hasClass("checked") ) {
+            $(this).removeClass("checked");
+            document.body.style.overflow = '';
+        } else {
+            $(this).addClass("checked");
+            document.body.style.overflow = 'hidden';
+        }
+    });
+});
