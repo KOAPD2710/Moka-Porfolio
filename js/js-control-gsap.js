2f02eb237f2cfb8b7a84af7de0c02812c8971379
Phan Đăng Khoa <khoaphan27102000@gmail.com>
2023-02-23 19:50:37 +0700

    Up

--- js/js-control-gsap.js
+++ js/js-control-gsap.js
@@ -8,6 +8,7 @@ $(document).ready(function(e){
 	gsap.registerPlugin(ScrollTrigger);
 	gsap.defaults({ease: "none",duration: 3});
 
+
 	gsap.to(".intro img", {
 		scrollTrigger: {
 			trigger: ".intro img",
@@ -17,9 +18,19 @@ $(document).ready(function(e){
 			markers: false,
 			id: "zoom",
 		},
-		scale: 1,
+		scale: "1",
 	});
-
+	gsap.to(".intro video", {
+		scrollTrigger: {
+			trigger: ".intro img",
+			start: "center 50%",
+			end: () => "+=" + height,
+			scrub: true,
+			markers: false,
+			id: "zoom",
+		},
+		scale: "1",
+	});
 	gsap.to(".intro img", {
 		scrollTrigger: {
 			trigger: ".intro-container",
@@ -29,7 +40,31 @@ $(document).ready(function(e){
 			markers: false,
 			id: "move",
 		},
-		y: 30*vh,
+		y: 50*vh,
+	});
+	gsap.to(".intro video", {
+		scrollTrigger: {
+			trigger: ".intro-container",
+			start: () => "+=" + height,
+			end: "bottom 0%",
+			scrub: 1,
+			markers: false,
+			id: "move",
+		},
+		y: 50*vh,
+	});
+
+	gsap.to(".moka-bandroll img", {
+		scrollTrigger: {
+			trigger: ".moka-bandroll",
+			start: "top bottom",
+			endTrigger: ".moka-folio .moka-bandroll",
+			end: "bottom 0%",
+			markers: false,
+			id: "moka-bandroll",
+		},
+		rotate: "-360",
+		repeat: "-1",
 	});
 
 	gsap.to(".moka-bandroll p", {
@@ -42,9 +77,9 @@ $(document).ready(function(e){
 			markers: false,
 			id: "moka-bandroll",
 		},
-		x: "-300*vw",
+		x: "-400*vw",
 	});
-	gsap.to(".moka-bandroll img", {
+	gsap.to(".moka-bandroll div", {
 		scrollTrigger: {
 			trigger: ".moka-bandroll",
 			start: "top bottom",
@@ -54,10 +89,20 @@ $(document).ready(function(e){
 			markers: false,
 			id: "moka-bandroll",
 		},
-		x: "-300*vw",
-		rotate: 360,
+		x: "-400*vw",
 	});
-
+	gsap.to(".folio-bandroll img", {
+		scrollTrigger: {
+			trigger: ".folio-bandroll",
+			start: "top bottom",
+			endTrigger: ".folio-folio .folio-bandroll",
+			end: "bottom 0%",
+			markers: false,
+			id: "folio-bandroll",
+		},
+		rotate: "360",
+		repeat: "-1",
+	});
 	gsap.to(".folio-bandroll p", {
 		scrollTrigger: {
 			trigger: ".folio-bandroll",
@@ -68,9 +113,9 @@ $(document).ready(function(e){
 			markers: false,
 			id: "folio-bandroll",
 		},
-		x: "300*vw",
+		x: "400*vw",
 	});
-	gsap.to(".folio-bandroll img", {
+	gsap.to(".folio-bandroll div", {
 		scrollTrigger: {
 			trigger: ".folio-bandroll",
 			start: "top bottom",
@@ -80,7 +125,37 @@ $(document).ready(function(e){
 			markers: false,
 			id: "folio-bandroll",
 		},
-		x: "300*vw",
-		rotate: 360,
+		x: "400*vw",
 	});
+	gsap.to(".test .sticky-text", {
+		scrollTrigger: {
+			trigger: ".test .sticky-text",
+			start: "center center",
+			endTrigger: ".test .showreel-container .last",
+			end: "center center",
+			pin: ".test .sticky-text",
+			markers: false,
+			id: "sticky-text",
+		}
+	})
+	//------------------Change Background------------------//
+	// gsap.to(".intro-container", {
+	// 	scrollTrigger: {
+	// 		trigger: ".intro",
+	// 		start:"bottom top",
+	// 		end: "top top",
+	// 		markers: true,
+	// 		id: "black",
+	// 	},
+	// 	backgroundColor: "black",
+	// });
+	// gsap.to(".intro-container", {
+	// 	scrollTrigger: {
+	// 		trigger: ".moka-folio",
+	// 		start:"bottom bottom",
+	// 		markers: false,
+	// 		id: "teel",
+	// 	},
+	// 	backgroundColor: "var(--t)",
+	// });
 })
--- js/js-index.js
+++ js/js-index.js
@@ -1,7 +1,38 @@
 $(document).ready(function(e){
+
+
+
 	var naviheight 		= $(".nav-container").height(),
 		navlinkheight	= $(".nav-bar").height();
-
 	$(".nav-bar").css("margin-top", ' ' + (naviheight - navlinkheight)/2 + 'px ');
 	$(".nav-bar").css("margin-bottom", ' ' + (naviheight - navlinkheight)/2 + 'px ');
-})
+
+
+	var flower1height 	= $(".moka-bandroll img").height(),
+		mokabandroll	= $(".moka-bandroll").height();
+	$(".moka-bandroll img").css("margin-top", ' ' + (mokabandroll - flower1height)/2 + 'px ');
+	$(".moka-bandroll img").css("margin-bottom", ' ' + (mokabandroll - flower1height)/2 + 'px ');
+
+	var flower2height 	= $(".folio-bandroll img").height(),
+		foliobandroll	= $(".folio-bandroll").height();
+	$(".folio-bandroll img").css("margin-top", ' ' + ((foliobandroll - flower2height)/2)*1.2 + 'px ');
+	// $(".folio-bandroll img").css("margin-bottom", ' ' + (foliobandroll - flower2height)/2 + 'px ');
+
+});
+
+window.addEventListener("resize", () => {
+	var width = $(window).innerWidth(),
+		height = $(window).innerHeight();
+	console.log(width, height);
+	if (width >= height) {
+		$("#horizontal-screen").css("display", "block");
+		$("#vertical-screen").css("display", "none");
+		console.log("horizontal-screen");
+	} else {
+		$("#horizontal-screen").css("display", "none");
+		$("#vertical-screen").css("display", "block");
+		console.log("vertical-screen");
+	}
+});
+
+
